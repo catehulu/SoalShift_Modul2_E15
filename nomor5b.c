a44ad93c8cd88db1e319a0ac555da2cedf2364ab
johnforjc <44581271+johnforjc@users.noreply.github.com>
2019-03-14 17:24:58 +0700

    nomor 5 tanpa nama program daemon

---
+++ nomor5b.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(){
+    // Cari PID nya executable file dari nomor 5a
+    FILE *pid_of_process;
+    pid_of_process=popen("pidof no5a", "r");
+
+    int pid_of_jlog;
+    fscanf(pid_of_process, "%d", &pid_of_jlog);
+
+    kill(pid_of_jlog,SIGKILL);
+
+    // langsung nama
+    // char *param_pkill={"pkill", "jlog", NULL};
+    // execv("/usr/bin/pkill", param_pkill);
+}
